371d217284b76811f0c94d97fcd51c731baa46b0
Owen <owen.lor.main@gmail.com>
2026-08-05 18:02:43 -0400

    Haved split the grid class and the boid class into seperate files. Boid returns in which cell it is from which is then used to determine the 8 surronding cells around it.

---
+++ grid.py
@@ -0,0 +1,35 @@
+import pygame
+from config import WIDTH, HEIGHT, CELL_SIZE, CELL_BORDER, NEIGHBOR_CELLS, DEEP_SLATE_GREY
+
+class SpatialGrid():
+    def __init__(self):
+        self.num_rows = int(WIDTH / CELL_SIZE)
+        self.num_cols = int(HEIGHT / CELL_SIZE)
+        self.cells = {}
+        self.reset()
+
+    def reset(self):
+        for x in range(self.num_rows):
+            for y in range(self.num_cols):
+                self.cells[(x, y)] = []
+
+    def draw(self, screen):
+        for x in range(self.num_rows):
+            for y in range(self.num_cols):
+                rect = (
+                    x * CELL_SIZE,
+                    y * CELL_SIZE,
+                    CELL_SIZE,
+                    CELL_SIZE
+                )
+                pygame.draw.rect(screen, DEEP_SLATE_GREY, rect, CELL_BORDER)
+
+    def surronding_boids(self, cell_x, cell_y):
+        surronding_boids = []
+        for x_offset, y_offset in NEIGHBOR_CELLS:
+            surronding_cell_x = (cell_x + x_offset) % self.num_rows
+            surronding_cell_y = (cell_y + y_offset) % self.num_cols
+            surronding_boids.extend(self.cells[(surronding_cell_x, surronding_cell_y)])
+        return surronding_boids
+
+    
